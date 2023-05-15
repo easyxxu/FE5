49470a9beabc51f44ebb5db180bfe0ede5f996e7
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-05-15 14:53:09 +0900

    20230515 - 기본 폴더 세팅

---
+++ React/20230515/myapp/src/App.js
@@ -0,0 +1,20 @@
+import Time from "./HookUseEffect/Quiz2";
+import Counter from "./HookUseEffect/TryUseEffect";
+import UseRef from "./HookUseRef/TryUseRef";
+import TryUseRefDom from "./HookUseRef/TryUseRefDom";
+import RouterQuiz from "./RouterSolve/RouterQuiz";
+// import RouterStudy from "./RouterSolve/RouterStudy";
+
+function App() {
+  return (
+    <div>
+      {/* <RouterStudy/> */}
+      {/* <RouterQuiz /> */}
+      {/* <Counter /> */}
+      {/* <Time /> */}
+      {/* <UseRef /> */}
+      <TryUseRefDom />
+    </div>
+  );
+}
+export default App;
